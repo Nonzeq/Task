ff05f08089ef275d7122e8fabc1484525ffc617c
Nonzeq <bgdn.kobylchak@gmail.com>
2022-01-24 23:35:37 +0300

    adsad

--- GUAtask/settings.py
+++ GUAtask/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lsj#)!gqj3gik*eb#d!r1j*-f6c*f#+q(f%)&jk=zelwcw!&9p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
